527d006edc033b7a2722a894fdd38fc177f12190
rogerof1 <rogerof1@TR12-103-18>
2016-10-05 09:45:20 +0200

    [warning] message on consola working, must change all logs

--- ROF_Engine/PanelConsole.cpp
+++ ROF_Engine/PanelConsole.cpp
@@ -20,6 +20,7 @@ void PanelConsole::Draw()
 		const char* item = Items[i];
 		ImVec4 col = ImVec4(1.0f, 1.0f, 1.0f, 1.0f); // A better implementation may store a type per-item. For the sample let's just parse the text. TODO
 		if (strstr(item, "[error]")) col = ImColor(1.0f, 0.4f, 0.4f, 1.0f); //If you write [error] before LOGs it will appear in red
+		else if (strstr(item, "[warning]")) col = ImColor(1.0f, 1.0f, 0.4f, 1.0f); //If you write [error] before LOGs it will appear in red
 		else if (strncmp(item, "# ", 2) == 0) col = ImColor(1.0f, 0.78f, 0.58f, 1.0f);
 		ImGui::PushStyleColor(ImGuiCol_Text, col);
 		ImGui::TextUnformatted(item);
