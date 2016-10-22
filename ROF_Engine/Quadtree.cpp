b9570b227a659e4e92de188b83934aaa44667add
RogerOlasz <rogerolasz96@gmail.com>
2016-10-22 11:19:16 +0200

    Doing quadtree...

---
+++ ROF_Engine/Quadtree.cpp
@@ -0,0 +1,7 @@
+#include "Quadtree.h"
+
+template<class Type>
+void QuadTreeNode::CollectIntersections(std::vector<GameObject*> &objects, const Type &primitive) const
+{
+	
+}
--- ROF_Engine/Quadtree.h
+++ ROF_Engine/Quadtree.h
@@ -1,12 +1,47 @@
 #ifndef __QUADTREE_H__
 #define __QUADTREE_H__
 
+#include "MathGeoLib/include/MathGeoLib.h"
+#include <vector>
+
+class GameObject;
+
+template<class Type>
+class QuadTreeNode
+{
+public:
+	QuadTreeNode<Type>*			parent;
+	QuadTreeNode<Type>*			children[4];
+	std::vector<Type*> bucket;
+
+public:
+
+	QuadTreeNode();
+
+	~QuadTreeNode();
+
+	void Insert();
+	void CollectIntersections(std::vector<GameObject*> &objects, const Type &primitive) const;
+
+};
+
+template<class Type>
 class QuadTree
 {
+public:
 	QuadTree();
 	~QuadTree();
 
+	void Create(AABB limits);
+	void Clear();
+
+	void Insert(GameObject* go);
+	void Remove(GameObject* go_to_delete);
+
+	bool Intersect(std::vector<GameObject*> &objects, Type &primitive);
+
 public:
+	QuadTreeNode<Type>* root;
 
 };
 
