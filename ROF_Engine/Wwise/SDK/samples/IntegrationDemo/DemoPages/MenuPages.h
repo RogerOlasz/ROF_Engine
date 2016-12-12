e6895b3f4b9a784d26e1abc22b21ce662076a2ef
RogerOlasz <rogerolasz96@gmail.com>
2016-12-12 18:02:42 +0100

    More Wwise files.

---
+++ ROF_Engine/Wwise/SDK/samples/IntegrationDemo/DemoPages/MenuPages.h
@@ -0,0 +1,46 @@
+/*******************************************************************************
+The content of this file includes portions of the AUDIOKINETIC Wwise Technology
+released in source code form as part of the SDK installer package.
+
+Commercial License Usage
+
+Licensees holding valid commercial licenses to the AUDIOKINETIC Wwise Technology
+may use this file in accordance with the end user license agreement provided 
+with the software or, alternatively, in accordance with the terms contained in a
+written agreement between you and Audiokinetic Inc.
+
+  Version: v2016.2.0  Build: 5972
+  Copyright (c) 2006-2016 Audiokinetic Inc.
+*******************************************************************************/
+
+// MenuPages.h
+/// \file 
+/// Include this file to have access to all Menu Pages.
+
+#pragma once
+
+#include "BaseMenuPage.h"
+#include "DemoRTPCCarEngine.h"
+#include "DemoDialogueMenu.h"
+#include "DemoDynamicDialogue.h"
+#include "DemoLocalization.h"
+#include "DemoMarkers.h"
+#include "DemoMusicCallbacks.h"
+#include "DemoInteractiveMusic.h"
+#include "DemoMIDIMetronome.h"
+#include "DemoMotion.h"
+#include "DemoExternalSources.h"
+#include "DemoFootstepsManyVariables.h"
+#include "DemoPositioningMenu.h"
+
+#if defined INTEGRATIONDEMO_MICROPHONE
+#include "DemoMicrophone.h"
+#endif 
+
+#ifdef AK_PS3
+#include "PS3/DemoRSX.h"
+#endif
+
+#if defined INTEGRATIONDEMO_BGM
+#include "DemoBGM.h"
+#endif
